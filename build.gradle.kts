f7883203732afe5a0f6148aae3f984a142680cf4
Samarium <28302241+Samarium150@users.noreply.github.com>
2022-02-24 12:47:45 +0800

    feat: initialize repository

---
+++ build.gradle.kts
@@ -0,0 +1,105 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.text.SimpleDateFormat
+import java.util.Date
+
+buildscript {
+    repositories {
+        maven("https://maven.minecraftforge.net")
+        mavenCentral()
+    }
+    dependencies {
+        classpath("net.minecraftforge.gradle:ForgeGradle:5.1.+") {
+            isChanging = true
+        }
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    java
+}
+apply(plugin="net.minecraftforge.gradle")
+
+group = "io.github.samarium150"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://thedarkcolour.github.io/KotlinForForge/")
+}
+
+dependencies {
+    "minecraft"("net.minecraftforge:forge:1.16.5-36.2.28")
+    implementation("thedarkcolour:kotlinforforge:1.16.0")
+}
+
+val Project.minecraft: net.minecraftforge.gradle.common.util.MinecraftExtension
+    get() = extensions.getByType()
+
+minecraft.let {
+    it.mappings("official", "1.16.5")
+    it.runs {
+        create("client") {
+            workingDirectory(project.file("run"))
+            property("forge.logging.console.level", "debug")
+            mods {
+                this.create("example-mod") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+        create("server") {
+            workingDirectory(project.file("run"))
+            property("forge.logging.console.level", "debug")
+            mods {
+                this.create("example-mod") {
+                    source(sourceSets.main.get())
+                }
+            }
+        }
+    }
+}
+
+tasks {
+    val javaVersion = JavaVersion.VERSION_1_8
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+        if (JavaVersion.current().isJava9Compatible) {
+            options.release.set(8)
+        }
+    }
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = javaVersion.toString()
+        }
+        sourceCompatibility = javaVersion.toString()
+        targetCompatibility = javaVersion.toString()
+    }
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(8))
+        }
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+    withType<Jar> {
+        archiveBaseName.set("example-mod")
+        manifest {
+            attributes(
+                mapOf(
+                    "Specification-Title" to project.name,
+                    "Specification-Vendor" to "author",
+                    "Specification-Version" to "1",
+                    "Implementation-Title" to project.name,
+                    "Implementation-Vendor" to "author",
+                    "Implementation-Version" to project.version,
+                    "Implementation-Timestamp" to SimpleDateFormat("yyyy-MM-dd").format(Date())
+                )
+            )
+        }
+        finalizedBy("reobfJar")
+    }
+}
---
+++ src/main/kotlin/io/github/samarium150/minecraft/mod/MineChronoArk.kt
@@ -0,0 +1,8 @@
+package io.github.samarium150.minecraft.mod
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(MineChronoArk.MOD_ID)
+object MineChronoArk {
+    const val MOD_ID = "mine_chrono_ark"
+}
